1944754c49a417c9d524a90fd5fa3d0104a2ad62
prachisinghsb <searchboxed-dev@SearchBoxeds-MacBook-Air.local>
2023-09-07 12:45:06 +0530

    object solved

---
+++ object/object.js
@@ -0,0 +1,12 @@
+
+
+// Object.keys - by this function you can directly get all the keys of that particular object.
+
+obj = {a:1,b:2,c:3}
+
+console.log(Object.keys(obj))
+
+// output --->
+
+
+    ['a','b','c']
